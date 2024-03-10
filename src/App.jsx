471d830d05ce2396450ac3a59d5f468ca2c3b172
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-03-10 05:55:00 +0300

    add: deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import HashLoader from "react-spinners/HashLoader";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
-function App () {
-
+function App() {
   //////////////////////////////
 
-
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
-////////////////////////////
+  ////////////////////////////
 
   useEffect(() => {
     const up = document.querySelectorAll(".up");
-  
+
     const handleScroll = () => {
       window.scrollY >= 100
         ? up.forEach((item) => item.classList.add("look"))
         : up.forEach((item) => item.classList.remove("look"));
     };
-  
+
     const handleScrollToTop = () => {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
     };
-  
+
     window.addEventListener("scroll", handleScroll);
     up.forEach((item) => item.addEventListener("click", handleScrollToTop));
-  
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      up.forEach((item) => item.removeEventListener("click", handleScrollToTop));
+      up.forEach((item) =>
+        item.removeEventListener("click", handleScrollToTop)
+      );
     };
   }, []);
 
@@ -46,7 +46,6 @@ function App () {
 
   useEffect(() => {
     setInterval(() => {
-      
       const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
@@ -55,38 +54,37 @@ function App () {
             entry.target.classList.remove("show");
           }
         });
+      });
+
+      const leftElements = document.querySelectorAll(".left");
+      const rightElements = document.querySelectorAll(".right");
+      const hiddenElements = document.querySelectorAll(".hidden");
+      const topElements = document.querySelectorAll(".top");
+      leftElements.forEach((el) => observer.observe(el));
+      rightElements.forEach((el) => observer.observe(el));
+      hiddenElements.forEach((el) => observer.observe(el));
+      topElements.forEach((el) => observer.observe(el));
+
+      return () => {
+        leftElements.forEach((el) => observer.unobserve(el));
+        rightElements.forEach((el) => observer.unobserve(el));
+        hiddenElements.forEach((el) => observer.unobserve(el));
+        topElements.forEach((el) => observer.unobserve(el));
+      };
     });
-    
-    const leftElements = document.querySelectorAll(".left");
-    const rightElements = document.querySelectorAll(".right");
-    const hiddenElements = document.querySelectorAll(".hidden");
-    const topElements = document.querySelectorAll(".top");
-    leftElements.forEach((el) => observer.observe(el));
-    rightElements.forEach((el) => observer.observe(el));
-    hiddenElements.forEach((el) => observer.observe(el));
-    topElements.forEach((el) => observer.observe(el));
-    
-    return () => {
-      leftElements.forEach((el) => observer.unobserve(el));
-      rightElements.forEach((el) => observer.unobserve(el));
-      hiddenElements.forEach((el) => observer.unobserve(el));
-      topElements.forEach((el) => observer.unobserve(el));
-    };
-  }, );
   }, []);
 
-////////////////////////////////
-const [loading , setLoading] = useState(false)
-  
-useEffect(()=>{
-  setLoading(true)
-  setTimeout(() => {
-    setLoading(false)
-  }, 5000);
-},[])
+  ////////////////////////////////
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
-////////////////////////////
+  ////////////////////////////
 
   return (
     <>
@@ -94,8 +92,8 @@ useEffect(()=>{
       {loading ? (
         <HashLoader
           style={{}}
-          className='loading'
-          color={'#2b8958'}
+          className="loading"
+          color={"#2b8958"}
           loading={loading}
           size={30}
           aria-label="Loading Spinner"
@@ -103,13 +101,11 @@ useEffect(()=>{
         />
       ) : (
         <>
-        <Router>
-<Routes>
-
-<Route path="/arabic_services_center/" element={<Home />} />
-      
-</Routes>
-        </Router>
+          <Router>
+            <Routes>
+              <Route path="/arabic_services_center/" element={<Home />} />
+            </Routes>
+          </Router>
         </>
       )}
     </>
@@ -117,4 +113,3 @@ useEffect(()=>{
 }
 
 export default App;
-
